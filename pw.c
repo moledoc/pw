fe26928f14a218595ce36c88c3dd555c80152579
Meelis Utt <meelis.utt@gmail.com>
2024-01-16 20:42:40 +0100

    re-write
    help is WIP

--- pw.c
+++ pw.c
@@ -8,8 +8,6 @@
 #define MOD 95
 #define var_name(var) #var
 
-#define LCG_A_MAX 1000
-#define LCG_C_MAX 10000
 #define LCG_M_MAX 100000
 
 enum mode {
@@ -21,10 +19,10 @@ enum mode {
 
 int MODE = MODE_SYM;
 
-// lng variables - arbitrarily selected to produce good enough pseudo-numbers
-int LCG_A = 127;
-int LCG_C = 5700;
-int LCG_M = 58595;
+// lcg variables
+int LCG_A = 0;
+int LCG_C = 0;
+int LCG_M = 0;
 
 size_t slen(const char *s) {
 	int i=0;
@@ -54,33 +52,41 @@ int eq(const char *s1, const char *s2) {
 	return 1;
 }
 
-int cpy(char *dest, char *src) {
+void cpy(char *dest, char *src) {
 	size_t src_len = slen(src);
 	if (src_len > MAX_SEED_SIZE) {
-		return 0;
+		errno = EINVAL;
+		return;
 	}
 	for (int i=0; i<src_len; ++i) {
 		dest[i] = src[i];
 	}
-	return 1;
+	return;
 }
 
-int a_i(char *ascii, int *integer) {
+int a_i(char *ascii) {
 	size_t ascii_len = slen(ascii);
-	if (!('0' <= ascii[ascii_len-1] && ascii[ascii_len-1] <= '9')) {
-		 return 0;
+	if (ascii_len == 0 || !('0' <= ascii[ascii_len-1] && ascii[ascii_len-1] <= '9')) {
+		errno = EINVAL;
+		return 0;
+	}
+	int until = 0;
+	int sign = 1;
+	if (ascii[0] == '-') {
+		until = 1;
+		sign = -1;
 	}
 	int tmp = ascii[ascii_len-1]-'0';
 	int mag = 1;
-	for (int i=ascii_len-1-1; i>=0; --i) {
+	for (int i=ascii_len-1-1; i>=until; --i) {
 		if (!('0' <= ascii[i] && ascii[i] <= '9')) {
+			errno = EINVAL;
 			return 0;
 		}
 		tmp += 10*mag*(ascii[i]-'0');
 		mag *= 10;
 	}
-	*integer = tmp;
-	return 1;
+	return sign*tmp;
 }
 
 int fit_mode(char c) {
@@ -120,17 +126,20 @@ void calc_pw(char *seed, char *domain, char *pw, size_t pw_size) {
 	int rs = gen_seedling(seed, seed_len);
 	size_t domain_len = slen(domain);
 	int rd = gen_seedling(domain, domain_len);
+	int nseed = rs*rd;
 
-	int iseed = rs*rd;
+	int failsafe = 0;
 	size_t i = 0;
 	for (;i<pw_size;) {
-		iseed = lcg(iseed);
-		char sc = ' ';
-		if (seed_len) {
-			sc = seed[i%seed_len];
+		if (failsafe >= 1000000) {
+			errno = ECANCELED;
+			return;
 		}
-		char c = (sc*(iseed+sc))%MOD+OFFSET;
+		char sc = seed[i%seed_len];
+		nseed = lcg(nseed);
+		char c = sc*(nseed+sc)%MOD+OFFSET;
 		if (!fit_mode(c)) {
+			++failsafe;
 			continue;
 		}
 		pw[i] = c;
@@ -138,25 +147,44 @@ void calc_pw(char *seed, char *domain, char *pw, size_t pw_size) {
 	}
 }
 
+void validate_lcg(int a, int c, int m) {
+	if (!(0 < m && m <= LCG_M_MAX)) {
+		errno = EINVAL;
+		return;
+	}
+	if (!(0 < a && a < LCG_M)) {
+		errno = EINVAL;
+		return;
+	}
+	if (!(0 < c && c < LCG_M)) {
+		errno = EINVAL;
+		return;
+	}
+}
+
 void help() {
 	printf("NAME\n\tpw - vaultless password manager\n");
-	printf("\nSYNOPSIS\n\tpw [-h] [-l LENGTH] [-m MODE] [--lng-a LCG_A] [--lng-c LCG_C] [--lng-m LCG_M] [-s SEED | -f SEEDFILE] DOMAIN\n");
+	printf("\nSYNOPSIS\n\tpw [-h] [-l LENGTH] [-m MODE] -lcg A C M -s SEED DOMAIN\n");
 	printf("\nOPTIONS\n");
-	printf("\t%-20s - print this help\n", "-h, --help, help");
-	printf("\t%-20s - specify length of password (default=%d)\n", "-l LENGTH", PW_SIZE);
-	printf("\t%-20s - specify password mode (default=%d)\n", "-m MODE", MODE_SYM);
-	printf("\t%-20s - specify password seed; only the first -s/-f is recognized\n", "-s SEED");
-	printf("\t%-20s - specify password seedfile; only the first -s/-f is recognized\n", "-f SEEDFILE");
-	printf("\t%-20s - specify LCG multiplier (default=%d); 0<LCG_A<=%d\n", "--lng-a LCG_A", LCG_A, LCG_A_MAX);
-	printf("\t%-20s - specify LCG constant (default=%d); 0<LCG_C<=%d\n", "--lng-c LCG_C", LCG_C, LCG_C_MAX);
-	printf("\t%-20s - specify LCG modulus (default=%d); %d<LCG_M<=%d\n", "--lng-m LCG_M", LCG_M, MOD, LCG_C_MAX);
-	printf("\t%-20s - specify the domain of the password; eg 'github.com/moledoc'\n", "DOMAIN");
+	printf("\t%s\n\t\tprint this help\n", "-h, -help, --help, help");
+	printf("\t%s\n\t\tspecify the domain of the password; eg 'github.com/moledoc'\n", "DOMAIN");
+	printf("\t%s\n\t\tspecify password seed:\n", "-s SEED");
+	printf("\t\t - if points to file, seed inside file\n");
+	printf("\t\t - else is seed\n");
+	printf("\t%s\n\t\tspecify LCG modifiers A, C, M:\n", "-lcg");
+	printf("\t\t - M (modulo), 0 < M < %s (%d)\n", var_name(LCG_M_MAX), LCG_M_MAX);
+	printf("\t\t - A (multiplier), 0 < A < M\n");
+	printf("\t\t - C (constant), 0 < C < M\n");
+	printf("\t%s\n\t\tspecify length of password (default=%d)\n", "-l LENGTH", PW_SIZE);
+	printf("\t%s\n\t\tspecify password mode (default=%d)\n", "-m MODE", MODE_SYM);
 	printf("\nMODES\n");
 	printf("\t%d (%s) - alphanumeric characters and symbols\n", MODE_SYM, var_name(MODE_SYM));
 	printf("\t%d (%s) - alphanumeric characters\n", MODE_NUM, var_name(MODE_NUM));
 	printf("\t%d (%s) - alphabet characters\n", MODE_UPP, var_name(MODE_UPP));
 	printf("\t%d (%s) - lowercase characters\n", MODE_LOW, var_name(MODE_LOW));
 	printf("\tanything else defaults to %s\n", var_name(MODE_SYM));
+	printf("\nEXAMPLES\n");
+	printf("\tpw -s test-test-test example@example.com/exampleuser\n");
 	printf("\nAUTHOR\n");
 	printf("\tMeelis Utt (meelis.utt@gmail.com)\n");
 }
@@ -166,85 +194,65 @@ int main(int argc, char **argv) {
 	zero(seed, MAX_SEED_SIZE);
 	char *domain = 0;
 	size_t pw_size = PW_SIZE;
+
+	// parse args
 	for (int i=1; i<argc; ++i) {
-		if (eq("-h", argv[i])|| eq("help", argv[i]) || eq("--help", argv[i])) {
+		char *flag = argv[i];
+		if (eq("-h", flag)|| eq("help", flag) || eq("-help", flag) || eq("--help", flag)) {
 			help();
 			return 0;
-		} else if (eq("-s", argv[i]) && i+1 < argc && *seed == '\0') {
-			int isvalid = cpy(seed, argv[i+1]);
-			if (!isvalid) {
-				fprintf(stderr, "[ERROR]: seed size (%ld) greater than allowed (%d)\n", slen(argv[i+1]), MAX_SEED_SIZE);
-				return EINVAL;
-			}
-			++i;
-		} else if (eq("-f", argv[i]) && i+1 < argc && *seed == '\0') {
+		} else if (eq("-s", flag) && i+1 < argc && *seed == '\0') {
 			FILE *fptr = fopen(argv[i+1], "r");
-			if (!fptr) {
-				fprintf(stderr, "[ERROR]: file '%s' does not exist\n", argv[i+1]);
-				return ENOENT;
+			if (fptr) {			
+				fseek(fptr, 0, SEEK_END);
+				long sf_size = ftell(fptr);
+				rewind(fptr);
+				if (sf_size > MAX_SEED_SIZE) {
+					errno = EINVAL;
+				} else {
+					fread(seed, sizeof(char), sf_size, fptr);
+				}
+				fclose(fptr);
+			} else {
+				errno = 0;
+				cpy(seed, argv[i+1]);
 			}
-			fseek(fptr, 0, SEEK_END);
-			long sf_size = ftell(fptr);
-			rewind(fptr);
-			if (sf_size > MAX_SEED_SIZE) {
-				fprintf(stderr, "[ERROR]: seed size (%ld) greater than allowed (%d)\n", sf_size, MAX_SEED_SIZE);
-				return EINVAL;
-			}
-			fread(seed, sizeof(char), sf_size, fptr);
-			fclose(fptr);
-			++i;
-		} else if (eq("-l", argv[i]) && i+1 < argc) {
-			int size;
-			int isvalid = a_i(argv[i+1], &size);
-			if (!isvalid) {
-				fprintf(stderr, "[ERROR]: invalid length provided\n");
-				return EINVAL;
-			}
-			pw_size = (size_t)size;
-			++i;
-		} else if (eq("-m", argv[i]) && i+1 < argc) {
-			a_i(argv[i+1], &MODE); // if was invalid nr, MODE is not changed and stays default, ie MODE_SYM
-			++i;
-		} else if (eq("--lng-a", argv[i]) && i+1 < argc) {
-			int isvalid = a_i(argv[i+1], &LCG_A);
-			if (!isvalid || !(0 < LCG_A && LCG_A <= LCG_A_MAX)) {
-				fprintf(stderr, "[ERROR]: invalid LCG multiplier provided\n");
-				return EINVAL;
-			}
-			++i;
-		} else if (eq("--lng-c", argv[i]) && i+1 < argc) {
-			int isvalid = a_i(argv[i+1], &LCG_C);
-			if (!isvalid || !(0 < LCG_C && LCG_C <= LCG_C_MAX)) {
-				fprintf(stderr, "[ERROR]: invalid LCG constant provided\n");
-				return EINVAL;
-			}
-			++i;
-		} else if (eq("--lng-m", argv[i]) && i+1 < argc) {
-			int isvalid = a_i(argv[i+1], &LCG_M);
-			if (!isvalid || !(MOD < LCG_M && LCG_M <= LCG_M_MAX)) {
-				fprintf(stderr, "[ERROR]: invalid LCG modulo provided\n");
-				return EINVAL;
-			}
-			++i;
+		} else if (eq("-l", flag) && i+1 < argc) {
+			pw_size = (size_t)a_i(argv[i+1]);
+		} else if (eq("-m", flag) && i+1 < argc) {
+			MODE = a_i(argv[i+1]);
+		} else if (eq("--lcg", flag) && i+3 < argc) {
+			LCG_A = a_i(argv[i+1]);
+			LCG_C = a_i(argv[i+2]);
+			LCG_M = a_i(argv[i+3]);
+			validate_lcg(LCG_A, LCG_C, LCG_M);
+			i+=2;
 		} else if (i == argc - 1) {
-			domain = argv[i];
-			break;
+			domain = flag;
 		} else {
-			fprintf(stderr, "[ERROR]: unsupported flag '%s'\n", argv[i]);
-			help();
-			return EINVAL;
+			errno = EINVAL;
 		}
+
+		if(errno) {
+			fprintf(stderr, "invalid argument at %s\nusage: pw [-h] [-l] [-m] -lcg a c m -s seed domain \nmore with `pw help`\n", flag);
+			return errno;
+		}
+		++i;
 	}
 
-
-	if (domain == NULL) {
-		fprintf(stderr, "[ERROR]: no domain provided\n");
+	// check required args
+	if (domain == NULL || *seed == '\0' || LCG_A == 0 || LCG_C == 0 || LCG_M == 0) {
+		fprintf(stderr, "required argument invalid or not provided\n");
 		return EINVAL;
 	}
-
+		
 	char pw[pw_size+1];
 	pw[pw_size] = '\0';
 	calc_pw(seed, domain, pw, pw_size);
+	if (errno) {
+		fprintf(stderr, "password calculation cancelled, loop detected. Please try other lcg values\n");
+		return errno;
+	}
 	puts(pw);
 	return 0;		
 }
