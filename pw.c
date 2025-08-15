e61fb5bca8076e18286fc90013aee92c3eb5c5a4
utt <meelis.utt@gmail.com>
2025-08-15 12:10:33 +0200

    update-interfaces: * domain requires flag * vault.contents 4th column is now digest_len explicitly

--- pw.c
+++ pw.c
@@ -5,15 +5,16 @@
 #define MD5_IMPLEMENTATION
 #include "md5.h"
 
-#define VERSION "0.5.3"
+#include "version.h"
 
 void help() {
   printf("NAME\n\tpw - vaultless password manager\n");
-  printf("\nSYNOPSIS\n\tpw [-h] [-s SALT] [-p PEPPER] -k KEY DOMAIN\n");
+  printf("\nSYNOPSIS\n\tpw [-h] [-s SALT] [-p PEPPER] [-l DIGEST_LENGTH] -k KEY DOMAIN\n");
   printf("\nOPTIONS\n");
   printf("\t%s\n\t\tprint help\n", "-h, -help, --help, help");
   printf("\t%s\n\t\tprint version\n", "-v, -version, --version, version");
   printf("\t%s\n\t\tkey file or literal (required)\n", "-k KEY, --key KEY");
+  printf("\t%s\n\t\tpassword domain (required)\n", "-d DOMAIN, --domain DOMAIN");
   printf("\t%s\n\t\tsalt the password - add something-something to the "
          "digestable message\n",
          "-s SALT, --salt SALT");
@@ -24,18 +25,17 @@ void help() {
          "allowed: 1-16);\n",
          "-l LENGTH, --length LENGTH");
   printf("\t\tNOTE: each digest element is 2 characters long\n");
-  printf("\t%s\n\t\tpassword domain (required)\n", "DOMAIN");
   printf("\nEXAMPLES\n");
   printf("\t* pw -h\n");
   printf("\t* pw help\n");
-  printf("\t* pw -k test example@example.com/exampleuser\n");
-  printf("\t* pw -k ./README.md example@example.com/exampleuser\n");
-  printf("\t* pw -k test -s test_test example@example.com/exampleuser\n");
-  printf("\t* pw -k test -p TEST_TEST example@example.com/exampleuser\n");
+  printf("\t* pw -k test -d example@example.com/exampleuser\n");
+  printf("\t* pw -k ./README.md -d example@example.com/exampleuser\n");
+  printf("\t* pw -k test -s test_test -d example@example.com/exampleuser\n");
+  printf("\t* pw -k test -p TEST_TEST -d example@example.com/exampleuser\n");
   printf("\t* pw -k test -s test_test -p TEST_TEST "
-         "example@example.com/exampleuser\n");
+         "-d example@example.com/exampleuser\n");
   printf("\t* pw -k ./README.md -s test_test -p TEST_TEST "
-         "example@example.com/exampleuser\n");
+         "-d example@example.com/exampleuser\n");
   printf("\nAUTHOR\n");
   printf("\tMeelis Utt (meelis.utt@gmail.com)\n");
 }
@@ -79,20 +79,18 @@ int main(int argc, char **argv) {
         memcpy(key, argv[i + 1], strlen(argv[i + 1]));
       }
       ++i;
-    } else if ((strcmp("-s", flag) == 0 || strcmp("--salt", flag) == 0) &&
-               i + 1 < argc) {
+    } else if ((strcmp("-d", flag) == 0 || strcmp("--domain", flag) == 0) && i + 1 < argc) {
+      domain = argv[i + 1];
+      ++i;
+    } else if ((strcmp("-s", flag) == 0 || strcmp("--salt", flag) == 0) && i + 1 < argc) {
       salt = argv[i + 1];
       ++i;
-    } else if ((strcmp("-p", flag) == 0 || strcmp("--pepper", flag) == 0) &&
-               i + 1 < argc) {
+    } else if ((strcmp("-p", flag) == 0 || strcmp("--pepper", flag) == 0) && i + 1 < argc) {
       pepper = argv[i + 1];
       ++i;
-    } else if ((strcmp("-l", flag) == 0 || strcmp("--length", flag) == 0) &&
-               i + 1 < argc) {
+    } else if ((strcmp("-l", flag) == 0 || strcmp("--length", flag) == 0) && i + 1 < argc) {
       length = atoi(argv[i + 1]);
       ++i;
-    } else if (i == argc - 1) {
-      domain = flag;
     } else {
       fprintf(stderr, "unexpected flag '%s'\n", flag);
       return 1;
---
+++ tests/run.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+filename="./inputs.txt"
+contents=$(split -p '\n' $filename)
+
+while IFS= read -r line; do
+    echo "$line" | /usr/bin/sed -E 's/^([^ ]+)? ?([^ ]+)? ?([^ ]+)? ?([0-9]+)?$/\1|\2|\3|\4/' | while IFS='|' read -r salt pepper domain digest_len; do
+        test -n "$salt" && salt="-s $salt"
+        test -n "$pepper" && pepper="-p $pepper"
+        test -n "$digest_len" && digest_len="-l $digest_len"
+        pw_old=$(pw_old -k "test" $salt $pepper $digest_len $domain | tr -d '\n')
+        test -n "$domain" && domain="-d $domain"
+        pw_new=$(pw -k "test" $salt $pepper $digest_len $domain | tr -d '\n')
+        test "$pw_old" = "$pw_new" || echo "diff:\n\tline: $line\n\told: $pw_old\n\tnew: $pw_new"
+    done
+done < $filename
+
+
+
