ed9473cb79be707acc7c55ae95ce60933a78500a
Meelis Utt <meelis.utt@gmail.com>
2025-08-15 12:43:20 +0200

    update-interfaces: (#7)
    * domain requires flag
    * vault.contents 4th column is now digest_len explicitly

--- A-Shell/README.md
+++ A-Shell/README.md
@@ -9,8 +9,8 @@ Download `vault` and `vault.py` in A-Shell app on iPhone.
 ## Dependencies
 
 * [pw](https://github.com/moledoc/pw)
-* file named `vault.contents` in the same directory as `vault` that contains salt, pepper, domain and extra flags in format:
-	* `salt pepper domain ("extra args")`; **NOTE:** extra args are optional and surrounded by double-quotes (")
+* file named `vault.contents` in the same directory as `vault` that contains salt, pepper, domain and optionally digest length in format:
+	* `salt pepper domain (digest_length)`
 
 ## Author
 
--- MacOS/vault.app/Contents/MacOS/vault.sh
+++ MacOS/vault.app/Contents/MacOS/vault.sh
@@ -3,19 +3,22 @@ SHELL=/bin/sh
 
 read -s -p "Master key: " pw_key
 echo
+echo
 
 cd /Applications/vault.app/Contents/MacOS
 contents=$(cat ./vault.contents)
 
-domain=$(printf "$contents" | /usr/bin/sed -E 's/^([^ ]+) ([^ ]+) ([^ ]+)( ".*")?$/\3/' | /opt/homebrew/bin/fzf)
+domain=$(printf "$contents" | /usr/bin/sed -E 's/^([^ ]+)? ?([^ ]+)? ?([^ ]+)? ?([0-9]+)?$/\3/' | /opt/homebrew/bin/fzf)
 escaped_domain=$(printf '%s' "$domain" | /usr/bin/sed -E 's/[][+/$*.^|\\]/\\&/g')
 
 line=$(printf "$contents" | /usr/bin/grep -E " $escaped_domain(\$| )")
-# [ "$(echo "$line" | wc -l)" -ne 1 ] && { echo "conflict"; sleep 5; exit 0}
 
 prev=$(pbpaste)
-echo "$line" | /usr/bin/sed -E 's/^([^ ]+) ([^ ]+) ([^ ]+)( ".*")?$/\1|\2|\3|\4/' | while IFS='|' read -r salt pepper domain extra; do
-	extra=$(echo $extra | sed 's/"//g')
-	/usr/local/bin/pw -k "$pw_key" -s $salt -p $pepper $extra $domain | tr -d '\n' | pbcopy
+echo "$line" | /usr/bin/sed -E 's/^([^ ]+)? ?([^ ]+)? ?([^ ]+)? ?([0-9]+)?$/\1|\2|\3|\4/' | while IFS='|' read -r salt pepper domain digest_len; do
+    test -n "$salt" && salt="-s $salt"
+	test -n "$pepper" && pepper="-p $pepper"
+	test -n "$domain" && domain="-d $domain"
+	test -n "$digest_len" && digest_len="-l $digest_len"
+	/usr/local/bin/pw -k $pw_key $salt $pepper $digest_len $domain | tr -d '\n' | pbcopy
 done
 (sleep 10; printf "$prev" | pbcopy) &
