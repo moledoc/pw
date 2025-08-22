5283f1d22b4bbb4f2037a1e61570ad26aa96eb0e
utt <meelis.utt@gmail.com>
2025-08-22 16:16:05 +0200

    pwgui: arrow up/down movements seems working

--- pwgui.c
+++ pwgui.c
@@ -540,12 +540,11 @@ int select_vault_content_idx(SDL_Window *window, SDL_Renderer *renderer, TTF_Fon
                     sdl_event.key.keysym.sym == SDLK_UP && 0 < selected_idx && selected_idx < print_textures_count) {
                 any_changes = true;
 
-                selected_idx -= 1;
                 if (0 < vertical_offset_idx && 
-                    print_these_textures[selected_idx]->rect->y-(vertical_offset_idx-1)*VAULT_CONTENTS_FONT_SIZE < 0) {
+                    print_these_textures[selected_idx]->rect->y <= 0) {
                     vertical_offset_idx -= 1;
                 }
-                
+                selected_idx -= 1;
             // END ARROW UP SELECT
 
             // START ARROW DOWN SELECT
@@ -575,11 +574,14 @@ int select_vault_content_idx(SDL_Window *window, SDL_Renderer *renderer, TTF_Fon
             goto vault_loop_delay;
         }
 
-
 vault_loop_render:
         SDL_RenderClear(renderer);
         int offset_h = 0;
         for (int i=vertical_offset_idx; i<print_textures_count; i++) {
+
+            SDL_Rect *corrected_rect = print_these_textures[i]->rect;
+            corrected_rect->y = offset_h;
+
             if (print_these_textures[i]->rect->y < 0 ||
                 window_h - INPUT_FONT_SIZE < offset_h + VAULT_CONTENTS_FONT_SIZE) {
                 break;
@@ -595,10 +597,8 @@ vault_loop_render:
                 SDL_SetRenderDrawColor(renderer, prev_renderer_color.r, prev_renderer_color.g, prev_renderer_color.b, prev_renderer_color.a); 
             }
 
-            SDL_Rect *corrected_rect = print_these_textures[i]->rect;
-            corrected_rect->y = offset_h;
-            offset_h += corrected_rect->h;
             SDL_RenderCopy(renderer, print_these_textures[i]->t, NULL, corrected_rect);
+            offset_h += corrected_rect->h;
         }
 
         {
